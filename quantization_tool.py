552218b41d3b8b0948d550f09ab6fdf535b25401
dongz666@outlook.com <dongzhiwei2021@outlook.com>
2019-09-29 15:38:58 +0800

    fix quantization main program include quantize parameters and inout

--- quantization_tool.py
+++ quantization_tool.py
@@ -19,21 +19,20 @@ from torch.utils.data import Dataset
 # -------------------------    param section    -------------------------
 
 parser = argparse.ArgumentParser(description='PyTorch Ristretto Quantization Tool')
-# bw bit_width 位宽
-# fl fraction_length 小数位长度
-# bool_q => is_quantize 是否量化
+# bit_width 位宽
+# fraction_length 小数位长度 仅在输入输出量化时起作用
+# is_quantize 是否量化 仅在输入输出量化时起作用
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
-model_q_path = './model/model_quan_704_xh_src.pth'  # pre-trained param
-model_param_path = './model/model_quan_704_xh_param.pth'  # 量化后的位置
-state = torch.load(model_q_path)  # pre-trained state
+pretrain_model_path = './model/model_quan_704_xh_src.pth'  # pre-trained param
+param_saving_path = './model/model_quan_704_xh_param.pth'  # 量化后参数存储的位置
+state = torch.load(pretrain_model_path)  # pre-trained state
 result_param = {}
 params = []
 state_tmp = state.copy()  # copy state for test after quantization
-bw_param = 8
-bw = 8
-fl = numpy.zeros(shape=(11))
-bool_q = numpy.zeros(shape=(11))
+bit_width = 8
+fraction_length = numpy.zeros(len(params))
+is_quantization = numpy.zeros(len(params))
 
 args = parser.parse_args()
 
@@ -155,73 +154,75 @@ def Trim2FixedPoint(data, bit_width=8, fl=0):
 
 
 # -------------------------    quantize params    -------------------------
-
+"""
+这个部分量化所有的参数，逐层量化，得到最佳量化策略之后测试精度并且保存最佳量化策略下的模型
+"""
 for layer in range(len(params)):  # 遍历所有的层数
-    acc_param = 0  # init acc
-    for fli_param in range(bw_param):  # 遍历所有的位宽
-        print('-------Quantize layer:{} parameter-------'.format(layer))
-        print('--------Quantize bit: {}---------'.format(fli_param))
+    acc_param = 0  # init accuracy
+    print('-------Quantizing layer:{} parameter-------'.format(layer))
+    for fraction_length_of_param in range(bit_width):  # 遍历所有的小数位置
+        print('--------Trying fraction length: {}---------'.format(fraction_length_of_param))
         for key in params[layer]:  # 量化制定的层
-            param = state[key].clone()
-            param = Trim2FixedPoint(param.float(), bw_param, fli_param)
-            state_tmp[key] = param
-        model_param = Net(bit_width=bw, fraction_length=fl, is_quantization=bool_q)
-        model_param.load_state_dict(state_tmp)
-        acc_param_new = evaluate(model_param, data_loader)  # test
-
-        if acc_param_new > acc_param:
-            result_param[layer] = [fli_param, acc_param_new]
+            param = state[key].clone()  # 提取特定层参数
+            param = Trim2FixedPoint(param.float(), bit_width, fraction_length_of_param)  # 量化
+            state_tmp[key] = param  # 修改临时参数中的指定层参数
+        model = Net(bit_width=bit_width,
+                    fraction_length=fraction_length,
+                    is_quantization=is_quantization)  # 模型实例化
+        model.load_state_dict(state_tmp)  # 加载参数
+        acc_param_eval = evaluate(model, data_loader)  # eval
+        if acc_param_eval > acc_param:
+            result_param[layer] = [fraction_length_of_param, acc_param_eval]  # 保存小数位置和精度
         else:
             result_param[layer] = result_param[layer]
+            # 如果没能获取更高的精度，恢复最好的参数
             for key_new in params[layer]:
                 param_new = state[key_new].clone()
-                param_new = Trim2FixedPoint(param_new.float(), bw_param, result_param[layer][0])
+                param_new = Trim2FixedPoint(param_new.float(), bit_width, result_param[layer][0])
                 state_tmp[key_new] = param_new
-
-        acc_param = max(acc_param, acc_param_new)
-        print('acc_param:', acc_param)
-    print('result_param:', result_param)
-state_test = state.copy()
-bw = 8
-for k, v in result_param.items():
-    for key in params[k]:
+        acc_param = max(acc_param, acc_param_eval)
+        print('--------Accuracy of fraction length: {} is {}---------'.format(fraction_length_of_param, acc_param))
+    print('-------Layer:{} parameter\'s best result is {} -------'.format(layer, result_param[layer][0]))
+final_state = state.copy()
+# 使用最佳量化策略，量化预训练模型
+for layer_num, _ in result_param.items():
+    for key in params[layer_num]:
         param = state[key].clone()
-        param = Trim2FixedPoint(param.float(), bw, result_param[k][0])
-        state_test[key] = param
-torch.save(state_test, model_param_path)  # 保存最佳策略下的参数
-model_param = Net(bit_width=bw, fraction_length=fl, is_quantization=bool_q)
-model_param.load_state_dict(state_test)
-acc_param_new = evaluate(model_param, data_loader)
-print(acc_param_new)  # 最佳策略下的精度
+        param = Trim2FixedPoint(param.float(), bit_width, result_param[layer_num][0])
+        final_state[key] = param
+model_param = Net(bit_width=bit_width,
+                  fraction_length=fraction_length,
+                  is_quantization=is_quantization)  # 实例化模型
+model_param.load_state_dict(final_state)  # eval
+acc_param_eval = evaluate(model_param, data_loader)  # get eval accuracy
+print('-------Quantize parameter is done, best accuracy is {} -------'.format(acc_param_eval))
 
-# -------------------------    quantize input & output    -------------------------
-
-result = numpy.zeros(shape=(11))
-for layer in range(len(bool_q)):  # 遍历层数
-    new_fl = fl
-    new_bool_q = bool_q  # .copy()
-    acc = 0
-    for fli in range(bw):
-        print('-------Quantize layer:{}-------'.format(layer))
-        print('-------Quantize bit:{}-------'.format(fli))
-        print('-------Test-------')
-        new_fl[layer] = fli
-        new_bool_q[layer] = 1
-        print('new_fl', new_fl)
-        print('new_bool_q', new_bool_q)
-        model = Net(bit_width=bw, fraction_length=fl, is_quantization=bool_q)
-        model.load_state_dict(state)
-        acc_new = evaluate(model, data_loader)
-        result[layer] = fli if acc_new > acc else result[layer]
-        new_fl[layer] = result[layer]
-        acc = max(acc, acc_new)
-        print(acc_new)
-best = result
-bool_q_new = numpy.ones_like(best)
-bw = 8
-model = Net(bit_width=bw, fraction_length=fl, is_quantization=bool_q)
-model.load_state_dict(state)
-acc = evaluate(model, data_loader)
+# -------------------------    quantize input && output    -------------------------
+"""
+这个部分是为了获得 fraction_length，这个参数是为模型定义量化的时候准备的
+"""
+for layer in range(len(is_quantization)):  # 遍历所有层
+    acc_param = 0  # init accuracy
+    for fraction_length_of_param in range(bit_width):  # 遍历所有的小数位
+        fraction_length[layer] = fraction_length_of_param
+        is_quantization[layer] = 1
+        model = Net(bit_width=bit_width,
+                    fraction_length=fraction_length,
+                    is_quantization=is_quantization)
+        model.load_state_dict(final_state)
+        acc_inout_eval = evaluate(model, data_loader)
+        fraction_length[layer] = fraction_length_of_param if acc_inout_eval > acc_param else fraction_length[layer]
+        acc_param = max(acc_param, acc_inout_eval)
+# test section
+is_quantization = numpy.ones_like(fraction_length)  # 返回一个和best一样尺寸的全1矩阵
+model = Net(bit_width=bit_width,
+            fraction_length=fraction_length,
+            is_quantization=is_quantization)
+model.load_state_dict(final_state)
+acc_inout_eval = evaluate(model, data_loader)
+print('-------Quantize inout is done, best accuracy is {} -------'.format(acc_inout_eval))
 
 # -------------------------    saving quantized model    -------------------------
-
+print("Saving quantized model.")
+torch.save(final_state, param_saving_path)  # 保存最佳策略下的参数
+print("Quantization finished!")
