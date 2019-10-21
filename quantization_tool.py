e11d7607e818070b817049784f057771ab072ea2
Albert Dong <dongz2177@icloud.com>
2019-10-21 22:27:12 +0800

    update get_params function add bn2scale function

--- quantization_tool.py
+++ quantization_tool.py
@@ -31,26 +31,76 @@ import utils
 
 
 # -------    param section    -------
-def get_params(state):
-    params = []
-    tmplist = []
-    tmpname = ''
-    # print(state)
-    for name in state:
-        if tmpname == '':
-            tmpname = name.split('.')[0:-1]
-            tmplist.append(name)
-        elif name.split('.')[-1] == "num_batches_tracked":
-            pass
-        elif tmpname == name.split('.')[0:-1]:
-            tmplist.append(name)
-        else:
-            params.append(tmplist)
-            tmplist = []
-            tmpname = name.split('.')[0:-1]
-            tmplist.append(name)
-    params.append(tmplist)
-    return params
+def bn2scale(model_state, layer_name):
+    """
+    description:
+        将指定bn层的函数改成scale层的形式，解决由PyTorch BN层的四个参数数量级不一致导致的在量化时的精度
+        较大损失，返回值为包含处理好的bn层的模型参数。
+        PyTorch BN数学公式：out = gamma * (x - mean)/sqrt(var) + beta
+        Caffe Scale 数学公式：out = alpha * x + beta
+        通过使得 new_gamma = gamma / sqrt(var),
+               new_beta = beta - gamma * mean / sqrt(var)
+               new_mean = 0, new_var = 1 来完成变换。P.S. 为了利用原来的BN的结构来实现scale。
+    :parameter
+        model_state:
+            未处理过的模型参数
+        layer_name: str
+            bn层的参数存储名字的layername，如cnn.batchnorm0，如果全名为cnn.batchnorm0.weight
+    :return:
+        model_state:
+            处理过的模型参数
+    """
+    # 从未处理的模型中获取bn层的四个参数
+    weight = model_state.get(layer_name + ".weight")
+    bias = model_state.get(layer_name + ".bias")
+    mean = model_state.get(layer_name + ".running_mean")
+    var = model_state.get(layer_name + ".running_var")
+    # 计算标准差，即sqrt(var) => std
+    std = torch.sqrt(var)
+    # 计算新的值
+    a = weight / std
+    b = bias - weight * mean / std
+    e = torch.zeros(mean.shape)
+    s = torch.ones(var.shape)
+    # 保存参数到模型
+    model_state[layer_name + ".weight"] = a
+    model_state[layer_name + ".bias"] = b
+    model_state[layer_name + ".running_mean"] = e
+    model_state[layer_name + ".running_var"] = s
+
+
+def get_params(model_state, is_scale):
+    """
+    description:
+        这个函数是暂时是专用函数，只能针对名称为layer_name.params_name 这种命名方式的提取。
+        比如："cnn.conv0.weight" 中 cnn.conv0 是layer_name, weight 是 param_name
+    :param model_state: 预训练模型参数
+    :param is_scale: 是否修改所有bn层到scale层表示
+    :return:
+        params_list: 可量量化的参数列表
+    """
+    # 初始化参数
+    params_list = []
+    for _, key in enumerate(model_state):
+        # 获取名称
+        layer_name = key.split('.')[0:-1]
+        param_name = key.split('.')[-1]
+        # 这里的判定还需要优化
+        # 如果指定需要优化，且当前层为bn层，且当前遍历参数为权重（只计算一次，防止出错）
+        # 调用bn2scale函数处理bn层。
+        if is_scale and 'batchnorm' in ''.join(layer_name) and 'weight' in ''.join(param_name):
+            bn2scale(model_state, '.'.join(layer_name))
+        # 如果 _ (index) 为0，说明列表中还什么都没有，将key.split()追加到列表后面
+        # 这是因为 key是一个字符串，key.split() 则是一个list
+        # 比如key = 'hello' key.split() = ['hello']
+        # 而 list(key) = ['h', 'e', 'l', 'l', 'o']
+        # 或者当前的layer_name 并非在现在的列表的最后一项list中，追加key.split()
+        if not _ or layer_name != params_list[-1][-1].split('.')[0:-1]:
+            params_list.append(key.split())
+        # 在其他的情况下，如果param_name不是"num_batches_tracked" 则追加到列表的最后一项中
+        elif param_name != "num_batches_tracked":
+            params_list[-1].append(key)
+    return params_list
 
 
 """
@@ -71,6 +121,8 @@ parser.add_argument('-b', '--bit_width', type=int, default=8,
                     help='number of bit you want to quantize pretrained model (default:8)')
 parser.add_argument('--gpu_id', default='0,1,2,3', type=str,
                     help='id(s) for CUDA_VISIBLE_DEVICES')
+parser.add_argument('--bn2scale', default=True, type=bool,
+                    help='Transfer BN to Scale')
 args = parser.parse_args()
 
 # Use CUDA
@@ -81,7 +133,7 @@ pretrain_model_path = args.pretrain  # 预训练模型参数路径
 param_saving_path = args.saving  # 量化后参数存储的位置
 bit_width = args.bit_width  # 量化的目标比特数
 state = torch.load(pretrain_model_path)  # 预训练模型参数
-params = get_params(state)
+params = get_params(state, args.bn2scale)
 fraction_length = numpy.zeros(len(params))
 is_quantization = numpy.zeros(len(params))
 state_tmp = state.copy()  # copy state for test after quantization
@@ -93,6 +145,7 @@ print("gpu id: " + str(args.gpu_id))
 print("bit width: " + str(args.bit_width))
 print("saving path: " + str(args.saving))
 print("pretrain path: " + str(args.pretrain))
+print("transfer BN to Scale: " + str(args.bn2scale))
 
 
 # -------    override quantization layer component    -------
@@ -154,7 +207,6 @@ class EltwiseMult(nn.Module):
                 res = self.quantization(res * t)
         return res
 
-# 这里是处理BN层的代码
 
 # -------    model section(custom needed)    -------
 
